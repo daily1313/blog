29c8fa43cfbc7d46f7dfa141b025a0aaeff185e1
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-01-13 16:09:06 +0900

    feat : KaKao OAuth 로그인에 필요한 클래스 추가 + JsonIgnoreProperties 어노테이션 추가

--- src/main/java/com/example/blog/domain/KakaoProfile.java
+++ src/main/java/com/example/blog/domain/KakaoProfile.java
@@ -1,15 +1,18 @@
 package com.example.blog.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class KakaoProfile {
-    public Integer id;
+    public Long id;
     public String connected_at;
     public Properties properties;
     public KakaoAccount kakao_account;
 
     @Data
+    @JsonIgnoreProperties(ignoreUnknown=true)
     public class Properties {
         public String nickname;
         public String profile_image;
@@ -17,6 +20,7 @@ public class KakaoProfile {
     }
 
     @Data
+    @JsonIgnoreProperties(ignoreUnknown=true)
     public class KakaoAccount {
         public Boolean profile_needs_agreement;
         public Profile profile;
@@ -26,7 +30,9 @@ public class KakaoProfile {
         public Boolean is_email_verified;
         public String email;
 
+
         @Data
+        @JsonIgnoreProperties(ignoreUnknown=true)
         public class Profile {
             public String nickname;
             public String thumbnail_image_url;

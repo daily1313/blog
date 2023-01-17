23d2e233b5b57a173ab9071c1b7cde57e52c554f
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-01-17 22:14:56 +0900

    Refactor : Security 설정 파일 수정

--- src/main/java/com/example/blog/config/auth/PrincipalDetail.java
+++ src/main/java/com/example/blog/config/auth/PrincipalDetail.java
@@ -9,8 +9,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 
-//스프리 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetail타입의 옵젝을
-// 스프링 시큐리티의 고유한 세션저장소에 저장함
+// 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails타입의 오브젝트를
+// 스프링 시큐리티의 고유한 세션 저장소에 저장함
 @Getter
 public class PrincipalDetail implements UserDetails {
 
@@ -20,31 +20,38 @@ public class PrincipalDetail implements UserDetails {
         this.user = user;
     }
 
-    @Override
-    public String getPassword() {
-        return null;
-    }
+
 
     @Override
     public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
         return user.getPassword();
     }
 
+    // 계정이 만료되지 않았는지 리턴한다. (true : 만료 안됨)
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
+
+    // 계정이 잠겨있지 않았는지 리턴한다.
 
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    //계정이 만료되지 않았는지 리턴한다.
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    // 계정이 활성화인지 리턴한다.
     @Override
     public boolean isEnabled() {
         return true;
@@ -54,8 +61,6 @@ public class PrincipalDetail implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> collection = new ArrayList<>();
         collection.add(() -> { return user.getRole().toString();});
-
-
         return collection;
     }
 }
--- src/main/java/com/example/blog/config/auth/PrincipalDetailService.java
+++ src/main/java/com/example/blog/config/auth/PrincipalDetailService.java
@@ -2,7 +2,9 @@ package com.example.blog.config.auth;
 
 import com.example.blog.domain.User;
 import com.example.blog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,7 +12,8 @@ import org.springframework.stereotype.Service;
 
 
 
-@Service
+@Service // Bean 등록
+
 public class PrincipalDetailService implements UserDetailsService {
 
     @Autowired
@@ -20,6 +23,7 @@ public class PrincipalDetailService implements UserDetailsService {
     // password 부분 처리는 알아서 함
     // username이 DB에 있는지만 확인해주면 됨
     // 밑에 Override 된 함수에서 username 확인을함
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User principal = userRepository.findByUsername(username)
@@ -27,5 +31,6 @@ public class PrincipalDetailService implements UserDetailsService {
                     return new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다. : " + username);
                 }); // Repository에서 반환이 Optional 이라서 orElseThrow 사용
         return new PrincipalDetail(principal);
+        // 시큐리티의 세션에 유저 정보가 저장이 된다. 아이디 : user, 패스워드 : 콘솔창(해쉬된 암호)
     }
 }

b58981d40f4bd7fd01d076789f99781e48057b4b
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-01-22 17:18:18 +0900

    feat : ExceptionHandler (500 error,Internal Server Error)

---
+++ src/main/java/com/example/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.blog.handler;
+
+import com.example.blog.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseDto<String> handleArgumentException(Exception e) {
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    } // 500 에러
+}
